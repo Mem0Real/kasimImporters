87ffa8d8c72a531136869012082d9e777231071e
Mem0Real <juniourmimo@gmail.com>
2024-05-24 01:37:57 +0300

    Minor alterations to the footer

--- src/app/(navigation)/Footer.jsx
+++ src/app/(navigation)/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
 	return (
-		<div className="h-16 bg-neutral-800 w-full text-neutral-200 text-base md:text-xl font-medium flex flex-col justify-center items-center">
+		<div className="h-16 bg-neutral-800 w-full text-neutral-200 text-sm font-light flex flex-col justify-center items-center">
 			<span>COPYRIGHT © 2023, KASIM KEMAL</span>
 			<span>All Rights Reserved</span>
 		</div>
