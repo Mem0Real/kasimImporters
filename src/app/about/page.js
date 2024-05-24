0ac74dccfa7a8d3643f422e5785e5958e8a94949
Mem0Real <juniourmimo@gmail.com>
2024-05-24 23:25:28 +0300

    Grid sort of works now

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,11 +3,10 @@ import React from "react";
 
 export default function About() {
 	const __dirName = "/assets/img/services";
-
 	return (
-		<div className="w-full flex flex-col justify-center items-center pt-[4em]">
+		<div className="min-h-screen w-full bg-neutral-300 pt-[4em]">
 			<div
-				className="h-[65vh] w-full flex flex-col justify-center items-start bg-center"
+				className="h-[75vh] w-full flex flex-col justify-center items-start bg-center"
 				style={{
 					background: 'url("/assets/img/about-bg.jpg") no-repeat center/cover',
 				}}
@@ -32,227 +31,175 @@ export default function About() {
 					</div>
 				</div>
 			</div>
-			<div className="h-full w-full bg-neutral-300 flex flex-col justify-center items-center">
-				<div className="max-w-[90vw] xl:max-w-[1100px] flex flex-col justify-center items-center">
-					<div className="w-full md:px-4 flex flex-col justify-center items-center gap-5 md:gap-2">
-						<h1 className="w-full text-2xl font-black text-neutral-800">
-							Our Services
-						</h1>
-						<div className="w-full flex flex-col md:flex-row gap-x-6 xl:gap-x-20 gap-y-6">
-							<div className="basis-3/4 md:basis-1/2 lg:basis-3/4 min-h-[30em] md:min-h-[35em] lg:min-h-96 w-full flex bg-white">
-								<div className="w-full flex flex-col lg:flex-row gap-4 md:gap-1 items-center">
-									<div className="py-4 pr-3 basis-4/5 w-[100%] h-[100%] lg:size-80 relative">
-										<Image
-											src={`${__dirName}/1.jpg`}
-											fill
-											alt="satisfaction"
-											className="absolute object-contain object-center"
-											sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-										/>
-									</div>
-									<div className="px-4 py-2 w-full basis-1/2 flex flex-col gap-2 justify-center items-start">
-										<div>
-											<p className="bg-purple-500 rounded-full text-[10px] p-2 text-neutral-200">
-												SATISFACTION
-											</p>
-										</div>
-										<div>
-											<h2 className="text-lg font-black text-neutral-800">
-												PROVIDING
-											</h2>
-										</div>
-										<div>
-											<h2 className="text-lg font-black text-neutral-800">
-												PROVIDING
-											</h2>
-										</div>
-									</div>
-								</div>
+			<div className="pt-8 space-t-2">
+				<div className="mt-10 w-[80vw] md:w-[90vw] lg:w-[95vw] xl:w-[75vw] xl:max-w-[1100px] md:px-8 lg:px-2 text-2xl font-black text-neutral-800 mx-auto grid grid-cols-1">
+					Our Services
+				</div>
+				<div className="w-[80vw] md:w-[90vw] lg:w-[95vw] xl:w-[75vw] xl:max-w-[1100px] mx-auto grid grid-cols-1 grid-flow-col md:grid-flow-row md:grid-cols-2 lg:grid-cols-3 grid-rows-7 md:grid-rows-4 lg:grid-rows-3 md:gap-x-4 xl:gap-x-16 gap-y-6 md:gap-y-2 lg:gap-y-4 xl:pr-16 xl:gap-y-8">
+					<div className="h-full px-2 md:px-4 lg:px-2 bg-white lg:col-span-2 grid grid-cols-1 lg:grid-cols-2 gap-4">
+						{/* Image */}
+						<div className="h-[15em] md:h-[20em] lg:h-[100%] xl:h-full w-[100%] relative">
+							<Image
+								src={`${__dirName}/1.jpg`}
+								fill
+								alt="satisfaction"
+								className="absolute object-contain object-center"
+								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							/>
+						</div>
+						{/* Text */}
+						<div className="px-2 grid grid-cols-1 items-center md:items-start place-items-start place-content-center gap-y-2">
+							<div>
+								<p className="bg-purple-500 rounded-full text-[10px] p-2 text-neutral-200">
+									SATISFACTION
+								</p>
 							</div>
-
-							<div className="w-full min-h-96 basis-1/4 md:basis-1/2 lg:basis-1/4 flex bg-stone-800 text-neutral-200">
-								<div className="py-4 px-5 w-full flex justify-evenly items-start">
-									<div className="flex flex-col gap-4 justify-center items-start">
-										<div>
-											<p className="bg-yellow-500 rounded-full text-[10px] p-2">
-												INNOVATIVE
-											</p>
-										</div>
-										<div>
-											<h2 className="text-lg font-black">BEING UNIQUE</h2>
-										</div>
-										<div>
-											<p className="text-base">
-												We have a deep passion for discovering unique and
-												innovative products that are not easily found in local
-												markets, offering our customers a truly exceptional
-												shopping experience.
-											</p>
-										</div>
-									</div>
-								</div>
+							<div>
+								<h2 className="text-lg font-black text-neutral-800">
+									PROVIDING
+								</h2>
+							</div>
+							<div>
+								<p className="text-base text-neutral-800">
+									Our mission is to bring the finest products from around the
+									world to our customers, ensuring their satisfaction and
+									delight.
+								</p>
 							</div>
 						</div>
-						<div className="w-full flex flex-col md:flex-row gap-x-6 lg:gap-x-7 xl:gap-x-20 gap-y-6">
-							<div className="h-[26em] lg:w-[35%] w-full flex gap-1 bg-white">
-								<div className="py-4 px-5 w-full flex flex-col gap-3 items-center">
-									<div className="basis-1/2 flex flex-col gap-2 justify-center items-start">
-										<div>
-											<p className="bg-yellow-500 rounded-full text-[10px] p-2 text-neutral-200">
-												EXPERIENCED
-											</p>
-										</div>
-										<div>
-											<h2 className="text-lg font-black text-neutral-800">
-												MANY SUPPLIERS
-											</h2>
-										</div>
-										<div>
-											<p className="text-base text-neutral-800">
-												With years of experience in the import industry, we have
-												developed strong relationships with reliable suppliers,
-												enabling us to consistently deliver high-quality
-												products to our customers.
-											</p>
-										</div>
-									</div>
-									<div className="py-4 pr-3 basis-4/5 w-[100%] h-[100%] lg:size-64 relative">
-										<Image
-											src={`${__dirName}/2.jpg`}
-											fill
-											alt="satisfaction"
-											className="absolute object-cover object-center"
-											sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-										/>
-									</div>
-								</div>
+					</div>
+					<div className="h-full p-4 grid grid-cols-1 items-center md:items-start place-items-start place-content-start gap-y-2 bg-[#333] text-neutral-200 ">
+						<div>
+							<p className="bg-yellow-500 rounded-full text-[10px] p-2">
+								INNOVATIVE
+							</p>
+						</div>
+						<div>
+							<h2 className="text-lg font-black">BEING UNIQUE</h2>
+						</div>
+						<div>
+							<p className="text-base">
+								We have a deep passion for discovering unique and innovative
+								products that are not easily found in local markets, offering
+								our customers a truly exceptional shopping experience.
+							</p>
+						</div>
+					</div>
+					<div className="min-h-[40vh] bg-white grid grid-cols-1 grid-rows-9 gap-y-2 px-2 py-5">
+						{/* Text */}
+						<div className="row-span-4 px-2 grid grid-cols-1 items-center md:items-start place-items-start place-content-center gap-y-2">
+							<div>
+								<p className="bg-yellow-500 rounded-full text-[10px] p-2 text-neutral-200">
+									EXPERIENCED
+								</p>
 							</div>
-							<div className="h-[26em] lg:w-[35%] px-5 py-4 w-full flex flex-col gap-3 items-center bg-white">
-								<div className="basis-1/2 py-4 pr-3 w-[100%] h-[100%] lg:size-64 relative">
-									<Image
-										src={`${__dirName}/4.jpg`}
-										fill
-										alt="satisfaction"
-										className="absolute object-cover object-center"
-										sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-									/>
-								</div>
-								<div className="basis-1/2 flex flex-col gap-2 justify-center items-start">
-									<div className="basis-1/2 flex flex-col gap-2 justify-center items-start">
-										<div>
-											<p className="bg-blue-500 rounded-full text-[10px] p-2 text-neutral-200">
-												QUALITY
-											</p>
-										</div>
-										<div>
-											<h2 className="text-lg font-black text-neutral-800">
-												CUSTOMER TRUST
-											</h2>
-										</div>
-										<div>
-											<p className="text-base text-neutral-800">
-												We understand the importance of quality control and
-												ensure that all products go through rigorous testing and
-												inspection before they reach our customers' hands.
-											</p>
-										</div>
-									</div>
-								</div>
+							<div>
+								<h2 className="text-lg font-black text-neutral-800">
+									MANY SUPPLIERS
+								</h2>
 							</div>
+							<div>
+								<p className="text-base text-neutral-800">
+									With years of experience in the import industry, we have
+									developed strong relationships with reliable suppliers,
+									enabling us to consistently deliver high-quality products to
+									our customers.
+								</p>
+							</div>
+						</div>
+						{/* Image */}
+						<div className="row-span-5 h-[15em] md:h-[20em] lg:h-[100%] xl:h-full w-[100%] relative">
+							<Image
+								src={`${__dirName}/2.jpg`}
+								fill
+								alt="satisfaction"
+								className="absolute object-cover object-center"
+								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							/>
+						</div>
+					</div>
+					<div className="min-h-[40vh] bg-white grid grid-cols-1 grid-rows-9 gap-y-2 px-2 py-5">
+						{/* Image */}
+						<div className="row-span-5 h-[15em] md:h-[20em] lg:h-[100%] xl:h-full w-[100%] relative">
+							<Image
+								src={`${__dirName}/4.jpg`}
+								fill
+								alt="satisfaction"
+								className="absolute object-cover object-center"
+								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							/>
+						</div>
+						{/* Text */}
+						<div className="row-span-4 px-2 grid grid-cols-1 items-center md:items-start place-items-start place-content-center gap-y-2">
+							<div>
+								<p className="bg-blue-500 rounded-full text-[10px] p-2 text-neutral-200">
+									QUALITY
+								</p>
+							</div>
+							<div>
+								<h2 className="text-lg font-black text-neutral-800">
+									CUSTOMER TRUST
+								</h2>
+							</div>
+							<div>
+								<p className="text-base text-neutral-800">
+									We understand the importance of quality control and ensure
+									that all products go through rigorous testing and inspection
+									before they reach our customers' hands.
+								</p>
+							</div>
+						</div>
+					</div>
 
-							<div className="w-full h-[26em] basis-1/4 flex bg-red-600 text-neutral-200">
-								<div className="py-4 px-5 w-full flex justify-evenly items-start">
-									<div className="flex flex-col gap-4 justify-center items-start">
-										<div>
-											<p className="bg-yellow-500 rounded-full text-[10px] p-2">
-												HONEST
-											</p>
-										</div>
-										<div>
-											<h2 className="text-lg font-black">BEST/FAIR PRICE</h2>
-										</div>
-										<div>
-											<p className="text-base">
-												We value transparency and honesty in our business
-												practices, providing our customers with accurate product
-												information and fair pricing.
-											</p>
-										</div>
-									</div>
-								</div>
+					<div className="bg-[#C72727] h-full p-4 grid grid-cols-1 items-center md:items-start place-items-start place-content-start gap-y-2">
+						<div>
+							<p className="bg-yellow-500 rounded-full text-[10px] p-2">
+								HONEST
+							</p>
+						</div>
+						<div>
+							<h2 className="text-lg font-black">BEST/FAIR PRICE</h2>
+						</div>
+						<div>
+							<p className="text-base">
+								We value transparency and honesty in our business practices,
+								providing our customers with accurate product information and
+								fair pricing.
+							</p>
+						</div>
+					</div>
+					<div className="hidden lg:block"></div>
+					<div className="h-fit col-span-2 px-2 md:px-4 lg:px-2 py-5 bg-white lg:col-span-2 grid grid-cols-1 lg:grid-cols-2  gap-4">
+						{/* Text */}
+						<div className=" md:row-span-1 px-2 grid grid-cols-1 items-center md:items-start place-items-start place-content-center gap-y-2">
+							<div>
+								<p className="bg-purple-500 rounded-full text-[10px] p-2 text-neutral-200">
+									RELATIONS
+								</p>
 							</div>
-
-							{/* <div className="h-[26em] lg:w-[35%] px-5 py-4 w-full flex flex-col gap-3 items-center bg-white">
-								<div className="basis-1/2 flex flex-col gap-2 justify-center items-start">
-									<div className="basis-1/2 flex flex-col gap-2 justify-center items-start">
-										<div>
-											<p className="bg-purple-500 rounded-full text-[10px] p-2 text-neutral-200">
-												RELATIONS
-											</p>
-										</div>
-										<div>
-											<h2 className="text-lg font-black text-neutral-800">
-												FAMILIAR SERVICES
-											</h2>
-										</div>
-										<div>
-											<p className="text-base text-neutral-800">
-												We believe in building long-term relationships with our
-												customers. Through personalized communication and
-												attentive customer support, we aim to create a loyal
-												customer base that trusts and relies on us for their
-												import needs.
-											</p>
-										</div>
-									</div>
-								</div>
-								<div className="basis-1/2 py-4 pr-3 w-[100%] h-[100%] lg:size-64 relative">
-									<Image
-										src={`${__dirName}/3.jpg`}
-										fill
-										alt="satisfaction"
-										className="absolute object-cover object-center"
-										sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-									/>
-								</div>
-							</div> */}
-							{/* <div className="flex w-full lg:pl-16 lg:pr-3">
-								<div className="hidden md:flex basis-1/3"></div>
-								<div className="w-full h-64 basis-1/2 flex bg-white">
-									<div className="py-4 pl-5 w-full flex flex-col md:flex-row gap-3 justify-between items-center">
-										<div className="basis-1/2 flex flex-col gap-2 justify-center items-start">
-											<div>
-												<p className="bg-purple-500 rounded-full text-[10px] p-2 text-neutral-200">
-													RELATIONS
-												</p>
-											</div>
-											<div>
-												<h2 className="text-lg font-black text-neutral-800">
-													FAMILIAR SERVICES
-												</h2>
-											</div>
-											<div>
-												<p className="text-base text-neutral-800">
-													We believe in building long-term relationships with
-													our customers. Through personalized communication and
-													attentive customer support, we aim to create a loyal
-													customer base that trusts and relies on us for their
-													import needs.
-												</p>
-											</div>
-										</div>
-										<div className="basis-1/2 size-60 relative">
-											<Image
-												src={`${__dirName}/3.jpg`}
-												fill
-												alt="satisfaction"
-												className="absolute object-cover object-center"
-												sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-											/>
-										</div>
-									</div>
-								</div>
-							</div> */}
+							<div>
+								<h2 className="text-lg font-black text-neutral-800">
+									FAMILIAR SERVICES
+								</h2>
+							</div>
+							<div>
+								<p className="text-base text-neutral-800">
+									We believe in building long-term relationships with our
+									customers. Through personalized communication and attentive
+									customer support, we aim to create a loyal customer base that
+									trusts and relies on us for their import needs.
+								</p>
+							</div>
+						</div>
+						{/* Image */}
+						<div className="h-[15em] md:h-[20em] lg:h-[100%] xl:h-full w-[100%] relative">
+							<Image
+								src={`${__dirName}/3.jpg`}
+								fill
+								alt="satisfaction"
+								className="absolute object-contain object-center"
+								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							/>
 						</div>
 					</div>
 				</div>
